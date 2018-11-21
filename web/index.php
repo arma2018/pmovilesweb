f3dac209197cbbf9b67dfd62d67136f7c098706b
arma2018 <45132213+arma2018@users.noreply.github.com>
2018-11-21 16:19:47 -0600

    Update index.php

--- web/index.php
+++ web/index.php
@@ -54,7 +54,7 @@
             }
             echo "</tbody></table>";
             echo "<script>
-                    setTimeout(function(){windows.location.reload();},5000);
+                    setTimeout(function(){window.location.reload();},5000);
                     </script>";
         }
         
